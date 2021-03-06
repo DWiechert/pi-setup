ed631719d297cf8748aa548f514a8a723fe1eab2
Dan Wiechert <dan.wiechert@gmail.com>
2021-03-06 08:28:16 -0600

    Create setup.sh

---
+++ nas/setup.sh
@@ -0,0 +1,6 @@
+ https://www.youtube.com/watch?v=U2w5PgJ4uhE
+ https://wiki.learnlinux.tv/index.php/Building_a_Raspberry-Pi_Storage_Server_with_OpenMediaVault
+ 
+ wget -O - https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install |sudo bash
+ 
+ ssh pi@pi-nas
