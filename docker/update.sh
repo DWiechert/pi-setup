9d03e5fda8ee725d8892833ea7eb82f58cd54288
Dan Wiechert <dan.wiechert@gmail.com>
2021-04-15 18:00:47 -0500

    Create update.sh

---
+++ docker/update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+cd /srv/dev-disk-by-uuid-5d345ba7-5cd8-46f5-a1a5-8835ab885c5d/docker/
+docker-compose pull
+docker-compose up -d
+docker image prune -f
