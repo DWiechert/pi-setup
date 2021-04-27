f9fee2cb85f1ee05f97a6ecdd174fb77d0fe00a9
Dan Wiechert <dan.wiechert@gmail.com>
2021-04-26 20:44:45 -0500

    Update update.sh

--- docker/update.sh
+++ docker/update.sh
@@ -1,9 +1,11 @@
-#!/bin/bash -x
+#!/bin/bash
+# sh /srv/dev-disk-by-uuid-5d345ba7-5cd8-46f5-a1a5-8835ab885c5d/storage2tb/Scripts/update.sh
+set -x
 
+cd ~
 sudo apt update
 sudo apt upgrade -y
-pushd /srv/dev-disk-by-uuid-5d345ba7-5cd8-46f5-a1a5-8835ab885c5d/docker/
+cd /srv/dev-disk-by-uuid-5d345ba7-5cd8-46f5-a1a5-8835ab885c5d/docker/
 docker-compose pull
 docker-compose up -d
 docker image prune -f
-popd
